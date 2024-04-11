aa0831a333d570f40a2b04ae2f1505a2cb5e01cc
pmc0707 <pranshugoodboy@gmail.com>
2024-04-11 11:15:13 +0530

    climbing stair in memoization rule and tabulation rule

--- src/dpImpProblems/ClimbingStairsMemoization.java
+++ src/dpImpProblems/ClimbingStairsMemoization.java
@@ -12,11 +12,25 @@ public class ClimbingStairsMemoization {
         ways[number] = countWays(number-1,ways) + countWays(number-2,ways);
         return ways[number];
     }
+    public static int countWaysTabulation(int number) {
+        int[] dp = new int[number+1];
+        dp[0] = 1;
+        //tabulation loop
+        for (int i = 1; i<=number; i++) {
+            if(i==1) {
+                dp[i] = dp[i - 1];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        return dp[number];
+    }
     public static void main(String[] args) {
         int number = 5;
         int[] ways = new int[number+1];
         Arrays.fill(ways, -1);
         System.out.println(countWays(number,ways));
+        System.out.println(countWaysTabulation(number));
     }
 }
 
